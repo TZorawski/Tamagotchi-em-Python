fa93c25df12d0e10aaf85f1643ca02939fda01c8
Thais Zorawski <thaiszorawski@gmail.com>
2019-07-15 21:02:42 -0300

    Inserindo comentários + Instruções README

--- README.md
+++ README.md
@@ -2,10 +2,13 @@
 Tamagotchi em Python desenvolvido para a disciplina de Linguagem de Programação do curso de Ciência da Computação da UTFPR de Campo Mourão.
 
 # É um programa de batatas. Olha os gifs de batatinhas :)
+Para executar use 'python home_screen.py'.
 
 ## Organização
+### home_screen.py
+Tela inicial do programa. É onde o usuário se identifica e escolhe qual Pet deseja abrir.
 ### tamagotchi.py
-Guarda a GUI do programa.
+Guarda a GUI do programa ligada ao Pet.
 
 ### potato.py
 Guarda tudo relacionado ao personagem, como estado, ações, etc.
--- home_screen.py
+++ home_screen.py
@@ -6,6 +6,7 @@ class HomeScreen:
     def __init__(self, parent, master=None):
         self.parent = parent
 
+        # Cria containers
         self.fontePadrao = ("Arial", "10")
         self.primeiroContainer = Frame(master)
         self.primeiroContainer["pady"] = 10
@@ -19,7 +20,8 @@ class HomeScreen:
         self.quartoContainer["pady"] = 20
         self.quartoContainer.pack()
   
-        self.titulo = Label(self.primeiroContainer, text="Dados do usuário")
+        # ======================== Preenche containers ========================
+        self.titulo = Label(self.primeiroContainer, text="Login")
         self.titulo["font"] = ("Arial", "10", "bold")
         self.titulo.pack()
   
@@ -31,26 +33,20 @@ class HomeScreen:
         self.nome["font"] = self.fontePadrao
         self.nome.pack(side=LEFT)
   
-        self.autenticar = Button(self.quartoContainer)
-        self.autenticar["text"] = "Entrar"
-        self.autenticar["font"] = ("Calibri", "8")
-        self.autenticar["width"] = 12
-        self.autenticar["command"] = self.login
-        self.autenticar.pack()
-  
-        self.mensagem = Label(self.quartoContainer, text="", font=self.fontePadrao)
-        self.mensagem.pack()
+        self.botao = Button(self.quartoContainer)
+        self.botao["text"] = "Entrar"
+        self.botao["font"] = ("Calibri", "8")
+        self.botao["width"] = 12
+        self.botao["command"] = self.login
+        self.botao.pack()
+
 
     # Entra na conta do usuario
     def login(self):
         usuario = self.nome.get()
-        # self.new_jan()
-        if True:
-            self.petWindow = tt.Toplevel(self.parent)
-            self.app = tamagotchi.Application(self.petWindow)
-            # self.petWindow.protocol("WM_DELETE_WINDOW", self.app.on_closing)
-        else:
-            self.petWindow.deiconify()
+        self.petWindow = tt.Toplevel(self.parent)
+        self.app = tamagotchi.Application(self.petWindow)
+        # self.petWindow.protocol("WM_DELETE_WINDOW", self.app.on_closing)
   
 
 def main():
